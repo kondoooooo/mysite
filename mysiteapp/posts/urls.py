13fd4271b19882e1eac596f908c35e5f704840bc
kshun <the.farthest.giant@gmail.com>
2019-11-30 05:04:11 +0900

    /posts/の表示

--- mysiteapp/posts/urls.py
+++ mysiteapp/posts/urls.py
@@ -1,6 +1,9 @@
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 # viewsのindexを呼び出して、関数の中を実行する
-urlpatterns = [url ( r'^$', views.index, name='index' )]
+# urlpatterns = [url ( r'^$', views.index, name='index' )]
+urlpatterns = [
+    path ( '', views.index, name='index' ),
+]
